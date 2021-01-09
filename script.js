6fb32342ed385222faac64e1b774bd656c9d6abb
herman088 <hermancho08@gmail.com>
2021-01-09 18:03:41 +0800

    Edit HTML and JAVASCRIPT to display results

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const searchForm = document.querySelector("form");
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
   console.log(searchQuery);
-    $.ajax(`https://api.edamam.com/search?q=${searchQuery}&app_id=93616471&app_key=00a798782e17b7f6a59dd7ed1b5656e9`
+    $.ajax(`https://api.edamam.com/search?q=${searchQuery}&app_id=93616471&app_key=00a798782e17b7f6a59dd7ed1b5656e9&from=0&to=20`
    
    )
         .done(function(response) {
@@ -21,3 +21,40 @@ const searchForm = document.querySelector("form");
         });
   
 });
+
+   
+
+
+
+function displayrecipes(results) {
+  
+  let addresults = "";
+  results.map((result) => {
+    addresults += `
+      <div class = "eachfood">
+            
+              <div class = "food1">
+                <div>
+                  <img src = ${result.recipe.image} alt = ${result.recipe.label}>
+
+                  <div class = "namefood">
+                    <h2 class "title"> ${result.recipe.label} </h2>
+                    <a href = ${result.recipe.url}> View Recipe </a>
+                  </div>
+
+                </div>
+              </div>
+              <p class  = "item-data"> Calories :  ${result.recipe.calories.toFixed(2)}  </p>
+              <p class  = "item-data"> Protein :  ${result.recipe.totalNutrients.PROCNT.quantity.toFixed(2)} ${result.recipe.totalNutrients.PROCNT.unit}</p>
+              <p class  = "item-data"> Carbs : ${result.recipe.totalNutrients.CHOCDF.quantity.toFixed(2)} ${result.recipe.totalNutrients.CHOCDF.unit} </p>
+            </div>
+          </div>
+    `;
+  });
+ eachfood.innerHTML = addresults;
+}
+
+
+
+
+
