c13c47735f28aeb171488ac1110d03f124341584
herman088 <hermancho08@gmail.com>
2021-01-10 21:41:35 +0800

    Add diet labels to result using javascript and html

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const searchForm = document.querySelector("form");
  
  let eachfood = document.querySelector(".eachfood")
 
-
+ let calorie =  document.querySelector(".Calorie")
 
   searchForm.addEventListener("submit", function(e){
   
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
-  console.log(searchQuery);
-    $.ajax(`https://api.edamam.com/search?q=${searchQuery}&app_id=93616471&app_key=00a798782e17b7f6a59dd7ed1b5656e9&from=0&to=20`
+
+  if(calorie.value != null)
+  {
+   url = `https://api.edamam.com/search?q=${searchQuery}&app_id=93616471&app_key=00a798782e17b7f6a59dd7ed1b5656e9&from=0&to=20&calories=${calorie.value}`
+  }
+  
+
+  if (calorie.value = ' '){
+
+  url =  `https://api.edamam.com/search?q=${searchQuery}&app_id=93616471&app_key=00a798782e17b7f6a59dd7ed1b5656e9&from=0&to=100`
+
+
+  }
+    $.ajax(url)
    
-   )
+   
         .done(function(response) {
-           console.log(response);
+            console.log(response.hits);
             displayrecipes(response.hits);
         }).fail(function() {
             console.log("error");
@@ -23,16 +35,13 @@ const searchForm = document.querySelector("form");
 });
 
    
-
-
-
 function displayrecipes(results) {
   
   let addresults = "";
   results.map((result) => {
     addresults += `
       <div class = "eachfood">
-            
+           
               <div class = "food1">
                 <div>
                   <img src = ${result.recipe.image} alt = ${result.recipe.label}>
@@ -44,17 +53,19 @@ function displayrecipes(results) {
 
                 </div>
               </div>
-              <p class  = "item-data"> Calories :  ${result.recipe.calories.toFixed(2)}  </p>
-              <p class  = "item-data"> Protein :  ${result.recipe.totalNutrients.PROCNT.quantity.toFixed(2)} ${result.recipe.totalNutrients.PROCNT.unit}</p>
-              <p class  = "item-data"> Carbs : ${result.recipe.totalNutrients.CHOCDF.quantity.toFixed(2)} ${result.recipe.totalNutrients.CHOCDF.unit} </p>
-            </div>
-          </div>
+              <div class  = "item-data"> Calories :  ${result.recipe.calories.toFixed(2)}  </div>
+              <div class  = "item-data"> Protein :  ${result.recipe.totalNutrients.PROCNT.quantity.toFixed(2)} ${result.recipe.totalNutrients.PROCNT.unit}</div>
+              <div class  = "item-data"> Carbs : ${result.recipe.totalNutrients.CHOCDF.quantity.toFixed(2)} ${result.recipe.totalNutrients.CHOCDF.unit} </div>
+              <div class  = "item-data">Diet Labels: ${result.recipe.dietLabels} </div>
+
+          
+
+
+
+    </div>
     `;
   });
  eachfood.innerHTML = addresults;
 }
-
-
-
 
 
